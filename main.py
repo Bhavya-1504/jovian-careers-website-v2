ec6afadcdaa4c2fe6d1f329d9215640338006b4d
Bhavya Bajaj <bhavyab1375@gmail.com>
2022-12-25 17:44:28 +0000

    dynamic pages for each job added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ main = Flask(__name__)
 @main.route("/")
 def hello_jovian():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs = jobs, company_name = 'Jovian')
+  return render_template('home.html', jobs = jobs)
 
 @main.route("/api/jobs")
 def list_jobs():
@@ -15,7 +15,10 @@ def list_jobs():
 @main.route('/job/<id>')
 def show_job(id):
   job = load_job_from_db(id)
-  return jsonify(job)
+  
+  if not job:
+    return"Not Found", 404
+  return render_template('jobpage.html', job=job)
   
 if __name__ == "__main__":
   main.run(host='0.0.0.0', debug=True)
